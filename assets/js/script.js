b589a94d7c7f7bad517be9af096d4164f07cab01
James Priestley <james.l.priestley@gmail.com>
2024-04-09 20:40:26 +0000

    Added divs to the structure of the HTML, added classes to the divs and started recreating the javascript so I can add modals rather than alerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,24 @@
+const userInput = document.getElementById("user-input");
+const input = document.getElementById("goal-input");
+
 /**
  * This will take the users input and display the text to the webapp
  * in form of a ul list.
  */
 function goalInput() {
-    let li = document.createElement("li");
-    let userInput = document.getElementById("userInput").value;
-    let text = document.createTextNode(userInput);
-    li.appendChild(text);
-  
-    if (userInput === '') {
-        alert("You have not entered a goal");
-    } else {
-        document.getElementById("goals").append(li);
-        let btn = document.createElement("button");
-        btn.innerHTML = "Complete!";
-        btn.onclick = function() {
-            alert("Congratulations on completing a task!");
-        };
-        document.getElementById("goals").append(btn);
+    if (userInput === ''){
+        
     }
 
-}
 
+/**
+ * This function will remove the users list item
+ * and provide a celebratory alert message
+ * once they have completed a task.
+ */
+function removeTask() {
+
+}
 
 /**
  * This function will tally up the users inputs 
@@ -40,14 +37,5 @@ function inputTally() {
  * and show the users outstanding/completed tasks
  */
 function completeTasks() {
-
-}
-
-/**
- * This function will remove the users list item
- * and provide a celebratory alert message
- * once they have completed a task.
- */
-function removeTask() {
 
 }
