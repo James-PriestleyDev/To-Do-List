004a5c262fdb2273f947a501a70ff24ad82371aa
James Priestley <james.l.priestley@gmail.com>
2024-04-14 19:49:42 +0000

    Added tally functionality. Which increments the 'Outstanding goals' score by 1 after every user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
+
+// calls a variable which selects all data-modal-target
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
+// calls a variable which selects all data-close-button. This will be used when adding a close function to the button.
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
+/**
+ * This creates the function for the modal to appear once an item is clicked.
+ */
 openModalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = document.querySelector(button.dataset.modalTarget)
@@ -9,6 +15,9 @@ openModalButtons.forEach(button => {
     })
 })
 
+/**
+ * This creates the functionanlity for the close button.
+ */
 closeModalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = button.closest('.modal')
@@ -22,18 +31,23 @@ closeModalButtons.forEach(button => {
     //overlay.classList.add('active')
 //}    
 
+/**
+ * This function will remove the modal from the screen once the user clicks the close button.
+ */
 function closeModal(modal) {
     if (modal == null ) return
    modal.classList.remove('active')
   overlay.classList.remove('active')
 }
 
+//These are const to tell the computer that these variables can not be changed and that they are constant.
 const userInput = document.getElementById("user-input");
 const input = document.getElementById("goal-input");
 
 /**
  * This will take the users input and display the text to the webapp
- * in form of a ul list.
+ * in form of a ul list. If there is no input then the user will be 
+ * presented with the modal.
  */
 function goalInput() {
     if (userInput.value === ''){
@@ -47,6 +61,7 @@ function goalInput() {
         let span = document.createElement("span");
         span.innerHTML = "Complete";
         li.appendChild(span);
+        inputTally();
     }
 }
 
@@ -58,22 +73,28 @@ function goalInput() {
  * once they have completed a task.
  */
 function removeTask() {
+    let completeGoal = document.getElementsByTagName("span");
+    completeGoal.addEventListener("click")
+    console.log("works!");
 
+    completeTasks();
 }
 
 /**
  * This function will tally up the users inputs 
- * and show the results on the webapp
+ * and show the results on the webapp (This was taken from "Love Maths" walkthrough on CI.)
  */
 function inputTally() {
-    
+    let outstanding = parseInt(document.getElementById("outstanding").innerHTML);
+    document.getElementById("outstanding").innerHTML = ++outstanding;
 
 }
 
 /**
  * This function will take the inputTally() function
- * and show the users outstanding/completed tasks
+ * and show the users completed tasks (This was taken from "Love Maths" walkthrough on CI.)
  */
 function completeTasks() {
-
+    let outstanding = parseInt(document.getElementById("completed").innerHTML);
+    document.getElementById("completed").innerHTML = ++outstanding;
 }
